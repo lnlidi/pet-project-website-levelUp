2bc6560abfb50e55487a4e8ffdcfc2cabd3f9d3b
Ayhal <ayhalvas2001@gmail.com>
2024-07-20 23:54:55 +1000

    v 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,70 @@
 
 
-// Scroll To Order
+// cookie
 
-function scrollToOrderSection() {
-    document.getElementById('order-section').scrollIntoView({ behavior: 'smooth' });
+function getCookie() {
+    return document.cookie.split('; ').reduce((acc, item) => {
+        const [name, value] = item.split('=')
+        acc[name] = value
+        return acc
+    }, {})
+}
+
+const cookie = getCookie()
+
+
+// Banner
+
+let stateBanner = cookie.stateBanner ? parseInt(cookie.stateBanner) : 1;
+const banner = document.querySelector('.banner');
+
+function checkBanner() {
+    banner.style.display = stateBanner ? null : 'none'
+}
+
+function hideBanner() {
+    stateBanner = 0
+    document.cookie = "stateBanner=0; max-age=" + (60 * 60); // 1 час = 60 * 60
+    checkBanner()
+}
+
+
+
+// Timer
+
+const daysDisplay = document.querySelector('.days');
+const hoursDisplay = document.querySelector('.hours');
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+
+let timer;
+let timeRemaining = localStorage.getItem('timeRemaining') 
+    ? parseInt(localStorage.getItem('timeRemaining')) 
+    : 60 * 60 * 24;
+
+function startTimer() {
+    if (timer) return; // предотвращаем запуск нескольких таймеров
+
+    timer = setInterval(() => {
+        if (timeRemaining <= 0) {
+            timeRemaining = 30 * 60
+        }
+        timeRemaining--;
+        localStorage.setItem('timeRemaining', timeRemaining);
+        updateTimerDisplay();
+    }, 1000);
+}
+
+function updateTimerDisplay() {
+    const days = Math.floor(timeRemaining / (60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((timeRemaining % (60 * 60)) / 60);
+    const seconds = timeRemaining % 60;
+
+    daysDisplay.textContent = `${days} д`;
+    hoursDisplay.textContent = `${hours} ч`;
+    minutesDisplay.textContent = `${minutes} м`;
+    secondsDisplay.textContent = `${seconds} с`;
 }
 
 
@@ -16,8 +77,12 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-window.addEventListener('scroll', function() {
-    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+
+function windowOnScroll() {
+    const scrolled = window.scrollY;
+
+    // scroll to top button
+    if (scrolled > 800) {
         if (!scrollToTopButtonIsShow) {
             scrollToTopButtonIsShow = true;
             scrollToTopButton.style.display = 'block';
@@ -26,7 +91,7 @@ window.addEventListener('scroll', function() {
         scrollToTopButtonIsShow = false;
         scrollToTopButton.style.display = null;
     }
-});
+}
 
 
 // Carousel
@@ -63,7 +128,7 @@ function swipeTo(index) {
     selectedItemIndex = index;
 }
 
-window.addEventListener('resize', function(event) {
+function windowOnResize() {
     height = window.innerHeight;
     width = window.innerWidth;
     
@@ -75,55 +140,58 @@ window.addEventListener('resize', function(event) {
     } else {
         isPhone = false;
     }
-}, true);
-
-
-// Timer
-
-const daysDisplay = document.querySelector('.days');
-const hoursDisplay = document.querySelector('.hours');
-const minutesDisplay = document.querySelector('.minutes');
-const secondsDisplay = document.querySelector('.seconds');
-
-let timer;
-let timeRemaining = localStorage.getItem('timeRemaining') 
-    ? parseInt(localStorage.getItem('timeRemaining')) 
-    : 30 * 60;
-
-function startTimer() {
-    if (timer) return; // предотвращаем запуск нескольких таймеров
-
-    if (timeRemaining <= 0) {
-        timeRemaining = 30 * 60
-    }
-    
-    timer = setInterval(() => {
-        timeRemaining--;
-        localStorage.setItem('timeRemaining', timeRemaining);
-        updateTimerDisplay();
-    }, 1000);
 }
-
-function updateTimerDisplay() {
-    const days = Math.floor(timeRemaining / (60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60));
-    const minutes = Math.floor((timeRemaining % (60 * 60)) / 60);
-    const seconds = timeRemaining % 60;
-
-    daysDisplay.textContent = `${days} д`;
-    hoursDisplay.textContent = `${hours} ч`;
-    minutesDisplay.textContent = `${minutes} м`;
-    secondsDisplay.textContent = `${seconds} с`;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    startTimer();
-    updateTimerDisplay();
-});
 
 
 // Tel Input
 
-function onInputTel(event) {
-    event.target.value = event.target.value.replace(/[^\d]/g, '');
+function onFocusTel(event) {
+    if (!event.target.value.startsWith('+7')) {
+        event.target.value = '+7 (';
+    }
 }
+
+function onBlurTel(event) {
+    if (event.target.value === '+7 (') {
+        event.target.value = '';
+    }
+}
+
+function onInputTel(event) {
+    const input = event.target.value;
+    const cleanInput = input.replace(/[^\d]/g, '');
+    const match = cleanInput.match(/(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})/);
+
+    if (match) {
+        event.target.value = `+7 (${match[2]}) ${match[3]}-${match[4]}-${match[5]}`;
+    } else {
+        event.target.value = `+7 (${cleanInput.slice(1, 4)}`;
+        if (cleanInput.length > 4) {
+            event.target.value += `) ${cleanInput.slice(4, 7)}`;
+        }
+        if (cleanInput.length > 7) {
+            event.target.value += `-${cleanInput.slice(7, 9)}`;
+        }
+        if (cleanInput.length > 9) {
+            event.target.value += `-${cleanInput.slice(9, 11)}`;
+        }
+    }
+
+    if (event.target.inputType === 'deleteContentBackward' && event.target.value.length < 4) {
+        event.target.value = '+7 (';
+    }
+}
+
+
+// onload
+
+document.addEventListener('DOMContentLoaded', function () {
+    checkBanner();
+    if (stateBanner) {
+        startTimer();
+        updateTimerDisplay();
+    }
+
+    window.addEventListener('scroll', windowOnScroll);
+    window.addEventListener('resize', windowOnResize, true);
+});
